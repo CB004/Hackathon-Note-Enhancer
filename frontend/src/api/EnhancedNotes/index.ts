887cbdcfaede99bcceb53a83dc3ce04e0ad3cd16
Sathira Katugaha <SGKat@SathBook-Pro.local>
2025-02-09 18:03:44 -0500

    fix axios usage

--- frontend/src/api/EnhancedNotes/index.ts
+++ frontend/src/api/EnhancedNotes/index.ts
@@ -9,10 +9,13 @@ const BASE_URL = import.meta.env.API_BASE_URL ?? "http://localhost:8000";
 export function generateEnhancedNotes(
   req: GenerateEnhancedNotesRequest
 ): Promise<GenerateEnhancedNotesResponse> {
-  return axios.post(`${BASE_URL}/hackathon/llm_tooling/notes_enhancement`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(req),
-  });
+  return axios.post(
+    `${BASE_URL}/hackathon/llm_tooling/notes_enhancement`,
+    req,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
