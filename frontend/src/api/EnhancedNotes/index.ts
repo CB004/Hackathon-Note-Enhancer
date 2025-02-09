ed86c410f1c68a6f37f77b006d14b6a521006963
Sathira Katugaha <SGKat@SathBook-Pro.local>
2025-02-09 18:04:44 -0500

    fix axios typing

--- frontend/src/api/EnhancedNotes/index.ts
+++ frontend/src/api/EnhancedNotes/index.ts
@@ -9,13 +9,15 @@ const BASE_URL = import.meta.env.API_BASE_URL ?? "http://localhost:8000";
 export function generateEnhancedNotes(
   req: GenerateEnhancedNotesRequest
 ): Promise<GenerateEnhancedNotesResponse> {
-  return axios.post(
-    `${BASE_URL}/hackathon/llm_tooling/notes_enhancement`,
-    req,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  return axios
+    .post<GenerateEnhancedNotesResponse>(
+      `${BASE_URL}/hackathon/llm_tooling/notes_enhancement`,
+      req,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((res) => res.data);
 }
